1b491dbd8095ed08f8adbe44a11451447b92558d
Sail100 <85443860+Sail100@users.noreply.github.com>
2024-01-18 16:45:53 -0500

    Update Install.lua
    Fix + Changed stuff

--- etruiav2/Install.lua
+++ etruiav2/Install.lua
@@ -1,39 +1,70 @@
--- Sail100
+-- Sail100, installer.lua
 
 local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
 
 local Window = OrionLib:MakeWindow({Name = "Etruia Installation", HidePremium = false, IntroText = "Etruia installion UI is loading.."})
 
+local lplr = game.Players.LocalPlayer
 
-
-
-local ETab = Window:MakeTab({
-	Name = "Install Etruia",
-	Icon = "rbxassetid://4483345998",
-	PremiumOnly = false
+local function notify(name, text, time)
+     OrionLib:MakeNotification({
+	Name = name,
+	Content = text,
+	Image = "rbxassetid://4483345998",
+	Time = time,
 })
-local UTab = Window:MakeTab({
-	Name = "Uninstall Etruia",
-	Icon = "rbxassetid://4483345998",
-	PremiumOnly = false
-})
+end
 
-ETab:AddButton({
-	Name = "Install V2",
-	Callback = function()
-      	print("button pressed")
+	if not isfile then
+		lplr:Kick("Executor does not support isfile, use Fluxus or Delta.")
+	end
+
+	if not readfile then
+		lplr:Kick("Executor does not support readfile, use Fluxus or Delta.")
+	end
+
+	if not makefolder then
+		lplr:Kick("Executor does not support makefolder, use Fluxus or Delta.")
+	end
+
+	if not writefile then
+		lplr:Kick("Executor does not support writefile, use Fluxus or Delta.")
+	end
+
+	if not delfile then
+		lplr:Kick("Executor does not support delfile, use Fluxus or Delta")
+	end
+end)
+
+local function install()
     local GUIPostions = game:HttpGet("https://raw.githubusercontent.com/Sail100/EtruiaConfigStorage/main/etruiav2/guiPos.txt")
     local profile = game:HttpGet("https://raw.githubusercontent.com/Sail100/EtruiaConfigStorage/main/etruiav2/profiles.txt")
     
     wait(.5)
     
-    -- notification
+    if not isfile('vape/Profiles/Render_Blurple6872274481.vapeprofile.txt') then
+     writefile(vape/Profiles/Render_Blurple6872274481.vapeprofile.txt', 'CreatedFile')
+ end
+ 
+  if not isfile('vape/Profiles/6872265039Render_BlurpleGUIPositions.vapeprofile.txt') then
+     writefile('vape/Profiles/6872265039Render_BlurpleGUIPositions.vapeprofile.txt', 'CreatedFile')
+ end
+    
+    OrionLib:MakeNotification({
+	Name = "Installation",
+	Content = "Installing...",
+	Image = "rbxassetid://4483345998",
+	Time = 3
+})
+
+task.wait(1)
     
     print('Installing..')
     
     delfile('vape/Profiles/Render_Blurple6872274481.vapeprofile.txt')
     delfile('vape/Profiles/6872265039Render_BlurpleGUIPositions.vapeprofile.txt')
-    
+
+
     wait(.5)
    
     OrionLib:MakeNotification({
@@ -45,6 +76,8 @@ ETab:AddButton({
  
     
     print('Writing Files..')
+    
+    task.wait(1)
     
     writefile('vape/Profiles/Render_Blurple6872274481.vapeprofile.txt', profile)
     print('Writen Profile')
@@ -62,8 +95,24 @@ ETab:AddButton({
 
   
     wait(.5)
-      
-      
+end
+
+local ETab = Window:MakeTab({
+	Name = "Install Etruia",
+	Icon = "rbxassetid://4483345998",
+	PremiumOnly = false
+})
+local UTab = Window:MakeTab({
+	Name = "Uninstall Etruia",
+	Icon = "rbxassetid://4483345998",
+	PremiumOnly = false
+})
+
+ETab:AddButton({
+	Name = "Install V2",
+	Callback = function()
+      	print("button pressed")
+                       install() 
   	end    
 })
 
@@ -76,7 +125,20 @@ UTab:AddButton({
    
     wait(.5)
     
-    -- notification
+       if not isfile('vape/Profiles/Render_Blurple6872274481.vapeprofile.txt') then
+     writefile(vape/Profiles/Render_Blurple6872274481.vapeprofile.txt', 'CreatedFile')
+ end
+ 
+  if not isfile('vape/Profiles/6872265039Render_BlurpleGUIPositions.vapeprofile.txt') then
+     writefile('vape/Profiles/6872265039Render_BlurpleGUIPositions.vapeprofile.txt', 'CreatedFile')
+ end
+    
+    OrionLib:MakeNotification({
+	Name = "Installation",
+	Content = "Uninstalling...",
+	Image = "rbxassetid://4483345998",
+	Time = 3
+})
     
     print('Uninstalling..')
     
@@ -86,7 +148,7 @@ UTab:AddButton({
     wait(3)
    
 OrionLib:MakeNotification({
-	Name = "Installion",
+	Name = "Installation",
 	Content = "Deleted old profiles.",
 	Image = "rbxassetid://4483345998",
 	Time = 7
