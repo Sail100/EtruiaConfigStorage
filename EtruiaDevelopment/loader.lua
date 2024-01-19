292926f71522a6262011766a6ac72f095d2f1446
Sail100 <85443860+Sail100@users.noreply.github.com>
2024-01-19 14:34:22 -0500

    Update loader.lua

--- EtruiaDevelopment/loader.lua
+++ EtruiaDevelopment/loader.lua
@@ -2,6 +2,8 @@
 
 -- THIS SCRIPT WILL BE FOR DEVELOPMENT AND OTHER PURPOSES.  EXPECT THIS NOT TO WORK SOMETIMES.
 
+print("loader")
+
 local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
 local Window = OrionLib:MakeWindow({Name = "Etruia Installation", HidePremium = false, IntroText = "Etruia installion UI is loading.."})
 local lplr = game.Players.LocalPlayer
@@ -12,6 +14,12 @@ local function notify(name, text, time)
 	Content = text,
 	Image = "rbxassetid://4483345998",
 	Time = time,
+})
+end
+
+function section(sectionname, tab, text)
+    local sectionname = tab:AddSection({
+	Name = text
 })
 end
 
@@ -157,6 +165,35 @@ end
 
 -- UI 
 
+local MTab = Window:MakeTab({
+	Name = " Main Tab",
+	Icon = "rbxassetid://4483345998",
+	PremiumOnly = false
+})
+
+
+
+section("StatsS01", MTab, "Welcome to Etruia's Installer.")
+section("StatsS02", MTab, "Here, you install Etruia.")
+section("StatsS03", MTab, "If you need help, look at the Tutorial Tab.")
+section("StatsS", MTab, "Stats:")
+section("StatsS1", MTab, "Username: " ..lplr.Name)
+section("StatsS2", MTab, "ID: " ..lplr.UserId)
+section("StatsS3", MTab, "Game Name: " ..game.Name)
+section("StatsS4", MTab, "Game ID: "..game.PlaceId)
+
+local TTab = Window:MakeTab({
+	Name = "Tutorial",
+	Icon = "rbxassetid://4483345998",
+	PremiumOnly = false
+})
+
+
+section("TTabS1", TTab, "**MAKE SURE YOU HAVE RENDER INSTALLED WITH PROFILES**")
+section("TTabS2", TTab, "To install Etruia, first go into the tab called: 'Install Etruia' ")
+section("TTabS3", TTab, "After, select the verison you want.")
+section("TTabS4", TTab, "Wait for it to finish installing and its done. Its that simple.")
+
 local ETab = Window:MakeTab({
 	Name = "Install Etruia",
 	Icon = "rbxassetid://4483345998",
@@ -247,3 +284,16 @@ OrionLib:MakeNotification({
   	end    
 })
 
+local UUTab = Window:MakeTab({
+	Name = "Updates",
+	Icon = "rbxassetid://4483345998",
+	PremiumOnly = false
+})
+
+
+section("UUTabUpd1", UUTab, "1-16-24 | Created github.")
+section("UUTabUpd2", UUTab, "1-17-24 | Added V2 Installion")
+section("UUTabUpd3", UUTab, "1-18-24 | Added V2.0.1 Installation")
+section("UUTabUpd4", UUTab, "1/19/24 | - Added Tabs [ Main Tab, Tutorial Tab, and Updates Tab. ] ")
+section("UUTabUpd5", UUTab, "1/19/24 | - Updated Installer ")
+UUTab:AddLabel("More updates coming soon!")
