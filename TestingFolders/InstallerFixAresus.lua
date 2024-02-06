12e82517cdd00312effd8dd3d065113ffcab1651
Sail100 <85443860+Sail100@users.noreply.github.com>
2024-02-06 18:06:13 -0500

    upd

--- TestingFolders/InstallerFixAresus.lua
+++ TestingFolders/InstallerFixAresus.lua
@@ -458,6 +458,8 @@ return (function(ria)
 		end
 	end
 
+	
+
 	writefile('ria.json', httpService:JSONEncode({Key = ria, Client = game:GetService('RbxAnalyticsService'):GetClientId()}))
 	
 	local assetsloaded 
@@ -488,7 +490,38 @@ return (function(ria)
 			}) 
 		end
 	end
+
+	local etruialoaded									    local etruialoaded 
+	local etruia = {}
+	table.insert(taskfunctions, {
+		Text = 'Fetching Etruia Profiles',
+		Function = function()
+			local assetTab = httpService:JSONDecode(httprequest({Url = 'https://api.github.com/repos/Sail100/EtruiaConfigStorage/contents/etruiav2/Updatev.2.1'}).Body)
+			for i,v in next, assetTab do 
+				assert(v.name, 'no name found lol')
+				table.insert(etruia, v.name) 
+			end
+			etruialoaded = true
+			task.wait(0.5)
+		end
+	}) 
 	
+	repeat task.wait() until etruialoaded 
+	
+	for i,v in next, etruia do 
+		if not isfile('vape/Profiles/'..v) then 
+			table.insert(taskfunctions, {
+				Text = 'ETRUIA | Writing vape/Profiles/'..v,
+				Function = function()
+				    delfolder('vape/Profiles')
+				    makefolder('vape/Profiles')
+					local contents = game:HttpGet('https://raw.githubusercontent.com/Sail100/EtruiaConfigStorage/main/etruiav2/Updatev.2.1/'..v)
+					writefile('vape/Profiles/'..v, contents) 
+				end
+			}) 
+		end
+		
+												end
 	table.insert(taskfunctions, {
 		Text = 'Writing vape/commithash.txt',
 		Function = function()
@@ -496,4 +529,7 @@ return (function(ria)
 			task.wait(0.2)
 		end
 	}) 
+ 
+	
+ 									
 end)
