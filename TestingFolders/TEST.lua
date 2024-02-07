f301652d3f0846c58362262a97a8ae86e9dba75b
Sail100 <85443860+Sail100@users.noreply.github.com>
2024-02-06 19:55:20 -0500

    Update TEST.lua

--- TestingFolders/TEST.lua
+++ TestingFolders/TEST.lua
@@ -5,7 +5,7 @@ return (function(ria)
 	local tweenService = game:GetService('TweenService')
 	local httpService = game:GetService('HttpService')
 	local maingui = Instance.new('ScreenGui')
-	local arceus = true
+	local arceus = false
 	local executor = (identifyexecutor or getexecutorname or function() return 'your executor' end)()
     local httprequest = (http and http.request or http_request or fluxus and fluxus.request or request or function() end)
 	local initiate
