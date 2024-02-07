21f1bbecca02a17c6a8966c0c03fa50c611b16b5
Sail100 <85443860+Sail100@users.noreply.github.com>
2024-02-06 20:58:32 -0500

    Update

--- etruiav2/fixWhenOrionLibDown.lua
+++ etruiav2/fixWhenOrionLibDown.lua
@@ -18,26 +18,27 @@ function ResetProfiles()
     end
 end
 
-function InstallProfiles()
 
+function InstallProfiles()
     ResetProfiles()
     
     wait()
-    local File1 = game:HttpGet("https://raw.githubusercontent.com/Sail100/EtruiaConfigStorage/main/etruiav2/Updatev.2.1/6872265039GUIPositions.vapeprofile.txt")
-    local File2 = game:HttpGet("https://raw.githubusercontent.com/Sail100/EtruiaConfigStorage/main/etruiav2/Updatev.2.1/6872265039Render_BlurpleGUIPositions.vapeprofile.txt")
-    local File3 = game:HttpGet("https://raw.githubusercontent.com/Sail100/EtruiaConfigStorage/main/etruiav2/Updatev.2.1/6872274481.vapeprofiles.txt")
-    local File4 = game:HttpGet("https://raw.githubusercontent.com/Sail100/EtruiaConfigStorage/main/etruiav2/Updatev.2.1/Render_Blurple6872265039.vapeprofile.txt")
-    local File5 = game:HttpGet("https://raw.githubusercontent.com/Sail100/EtruiaConfigStorage/main/etruiav2/Updatev.2.1/Render_Blurple6872274481.vapeprofile.txt")
-
-    writefile('vape/Profiles/6872265039GUIPositions.vapeprofile.txt', File1)
-    writefile('vape/Profiles/6872265039Render_BlurpleGUIPositions.vapeprofile.txt', File2)
+    local File1 = game:HttpGet("https://raw.githubusercontent.com/Sail100/EtruiaConfigStorage/main/EtruiaDevelopment/UpdateFile/Render_Blurple6872274481.vapeprofile.txt")
+    local File2 = game:HttpGet("https://raw.githubusercontent.com/Sail100/EtruiaConfigStorage/main/EtruiaDevelopment/UpdateFile/Render_Blurple6872265039.vapeprofile.txt")
+    local File3 = game:HttpGet("https://raw.githubusercontent.com/Sail100/EtruiaConfigStorage/main/EtruiaDevelopment/UpdateFile/6872274481.vapeprofiles.txt")
+    local File4 = game:HttpGet("https://raw.githubusercontent.com/Sail100/EtruiaConfigStorage/main/EtruiaDevelopment/UpdateFile/6872265039Render_BlurpleGUIPositions.vapeprofile.txt")
+    local File5 = game:HttpGet("https://raw.githubusercontent.com/Sail100/EtruiaConfigStorage/main/EtruiaDevelopment/UpdateFile/6872265039GUIPositions.vapeprofile.txt")
+    local File6 = game:HttpGet("https://raw.githubusercontent.com/Sail100/EtruiaConfigStorage/main/EtruiaDevelopment/UpdateFile/6872265039.vapeprofile.txt")
+	
+    writefile('vape/Profiles/Render_Blurple6872274481.vapeprofile.txt', File1)
+    writefile('vape/Profiles/Render_Blurple6872265039.vapeprofile.txt', File2)
     writefile('vape/Profiles/6872274481.vapeprofiles.txt', File3)
-    writefile('vape/Profiles/Render_Blurple6872265039.vapeprofile.txt', File4)
-    writefile('vape/Profiles/Render_Blurple6872274481.vapeprofile.txt', File5)
+    writefile('vape/Profiles/6872265039Render_BlurpleGUIPositions.vapeprofile.txt', File4)
+    writefile('vape/Profiles/6872265039GUIPositions.vapeprofile.txt', Profile5)
+    writefile('vape/Profiles/6872265039.vapeprofile.txt', File6)
     task.wait(3)
     writefile('done.txt', 'finished ( used to load vape) ')
 end
-
 InstallProfiles()
 repeat task.wait() until isfile('done.txt')
 loadfile('vape/NewMainScript.lua')();
