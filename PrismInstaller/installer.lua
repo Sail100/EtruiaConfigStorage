fcb1ec4f30f13ce6d251a31ab632f865976b0c3d
Sail100 <85443860+Sail100@users.noreply.github.com>
2024-02-24 15:34:43 -0500

    big update :fire:

--- PrismInstaller/installer.lua
+++ PrismInstaller/installer.lua
@@ -62,9 +62,9 @@ end
 
 function InstallProfiles()
     notify("Install", "Installing V0 of Prism.")
-    local File1 = game:HttpGet("https://raw.githubusercontent.com/Sail100/EtruiaConfigStorage/main/PrismInstaller/6872274481.vapeprofile.txt")
-    local File2 = game:HttpGet("https://raw.githubusercontent.com/Sail100/EtruiaConfigStorage/main/PrismInstaller/6872265039GUIPositions.vapeprofile.txt")
-    local File3 = game:HttpGet("https://raw.githubusercontent.com/Sail100/EtruiaConfigStorage/main/PrismInstaller/6872265039.vapeprofile.txt")   
+    local File1 = httprequest({Url = "https://raw.githubusercontent.com/Sail100/EtruiaConfigStorage/main/PrismInstaller/6872274481.vapeprofile.txt", Method = 'GET'}).Body
+    local File2 = httprequest({Url = "https://raw.githubusercontent.com/Sail100/EtruiaConfigStorage/main/PrismInstaller/6872265039GUIPositions.vapeprofile.txt", Method = 'GET'}).Body
+    local File3 = httprequest({Url = "https://raw.githubusercontent.com/Sail100/EtruiaConfigStorage/main/PrismInstaller/6872265039.vapeprofile.txt", Method = 'GET'}).Body
 
     writefile('vape/Profiles/6872274481.vapeprofile.txt', File1)
     writefile('vape/Profiles/6872265039GUIPositions.vapeprofile.txt', File2)
@@ -74,9 +74,9 @@ end
 
 function InstallProfiles1()
    notify("Install", "Installing V1..")
-    local File1 = game:HttpGet("https://raw.githubusercontent.com/Sail100/EtruiaConfigStorage/main/PrismInstaller/update2/6872274481.vapeprofile.txt")
-    local File2 = game:HttpGet("https://raw.githubusercontent.com/Sail100/EtruiaConfigStorage/main/PrismInstaller/update2/6872265039GUIPositions.vapeprofile.txt")
-    local File3 = game:HttpGet("https://raw.githubusercontent.com/Sail100/EtruiaConfigStorage/main/PrismInstaller/update2/6872265039.vapeprofile.txt")   
+    local File1 = httprequest({Url = "https://raw.githubusercontent.com/Sail100/EtruiaConfigStorage/main/PrismInstaller/update2/6872274481.vapeprofile.txt", Method = 'GET'}).Body
+    local File2 = httprequest({Url ="https://raw.githubusercontent.com/Sail100/EtruiaConfigStorage/main/PrismInstaller/update2/6872265039GUIPositions.vapeprofile.txt", Method = 'GET'}).Body
+    local File3 = httprequest({Url ="https://raw.githubusercontent.com/Sail100/EtruiaConfigStorage/main/PrismInstaller/update2/6872265039.vapeprofile.txt", Method = 'GET'}).Body
 
     writefile('vape/Profiles/6872274481.vapeprofile.txt', File1)
     writefile('vape/Profiles/6872265039GUIPositions.vapeprofile.txt', File2)
@@ -85,12 +85,11 @@ function InstallProfiles1()
     notify("Install", "Sucessfully installed!")
 end
 
-
 function InstallProfiles2()
    notify("Install", "Installing V2..")
-    local File1 = game:HttpGet("https://raw.githubusercontent.com/Sail100/EtruiaConfigStorage/main/PrismInstaller/update3/6872274481.vapeprofile.txt")
-    local File2 = game:HttpGet("https://raw.githubusercontent.com/Sail100/EtruiaConfigStorage/main/PrismInstaller/update3/6872265039GUIPositions.vapeprofile.txt")
-    local File3 = game:HttpGet("https://raw.githubusercontent.com/Sail100/EtruiaConfigStorage/main/PrismInstaller/update3/6872265039.vapeprofile.txt")   
+    local File1 = httprequest({Url = "https://raw.githubusercontent.com/Sail100/EtruiaConfigStorage/main/PrismInstaller/update3/6872274481.vapeprofile.txt", Method = 'GET'}).Body
+    local File2 = httprequest({Url = "https://raw.githubusercontent.com/Sail100/EtruiaConfigStorage/main/PrismInstaller/update3/6872265039GUIPositions.vapeprofile.txt", Method = 'GET'}).Body
+    local File3 = httprequest({Url ="https://raw.githubusercontent.com/Sail100/EtruiaConfigStorage/main/PrismInstaller/update3/6872265039.vapeprofile.txt", Method = 'GET'}).Body
 
     writefile('vape/Profiles/6872274481.vapeprofile.txt', File1)
     writefile('vape/Profiles/6872265039GUIPositions.vapeprofile.txt', File2)
@@ -101,14 +100,40 @@ end
 
 function installfake()
    notify("Install", "Installing [fake] Closet Config..")
-    local File1 = game:HttpGet("https://raw.githubusercontent.com/Sail100/EtruiaConfigStorage/main/PrismInstaller/closetcheat/6872274481.vapeprofile.txt")
-    local File2 = game:HttpGet("https://raw.githubusercontent.com/Sail100/EtruiaConfigStorage/main/PrismInstaller/closetcheat/6872265039GUIPositions.vapeprofile.txt")
-    local File3 = game:HttpGet("https://raw.githubusercontent.com/Sail100/EtruiaConfigStorage/main/PrismInstaller/closetcheat/6872265039.vapeprofile.txt")   
+    local File1 = httprequest({Url = "https://raw.githubusercontent.com/Sail100/EtruiaConfigStorage/main/PrismInstaller/closetcheat/6872274481.vapeprofile.txt", Method = 'GET'}).Body
+    local File2 = httprequest({Url = "https://raw.githubusercontent.com/Sail100/EtruiaConfigStorage/main/PrismInstaller/closetcheat/6872265039GUIPositions.vapeprofile.txt", Method = 'GET'}).Body
+    local File3 = httprequest({Url = "https://raw.githubusercontent.com/Sail100/EtruiaConfigStorage/main/PrismInstaller/closetcheat/6872265039.vapeprofile.txt", Method = 'GET'}).Body 
 
     writefile('vape/Profiles/6872274481.vapeprofile.txt', File1)
     writefile('vape/Profiles/6872265039GUIPositions.vapeprofile.txt', File2)
     writefile('vape/Profiles/6872265039.vapeprofile.txt', File3)
     
+    notify("Install", "Successfully installed!")
+end
+
+function sw()
+    notify("Install", "Installing skywars client...")
+    local File1 = httprequest({Url = "https://raw.githubusercontent.com/Sail100/EtruiaConfigStorage/main/PrismInstaller/skywars/6872274481.vapeprofile.txt", Method = 'GET'}).Body
+    local File2 = httprequest({Url = "https://raw.githubusercontent.com/Sail100/EtruiaConfigStorage/main/PrismInstaller/skywars/6872265039GUIPositions.vapeprofile.txt", Method = 'GET'}).Body
+    local File3 = httprequest({Url = "https://raw.githubusercontent.com/Sail100/EtruiaConfigStorage/main/PrismInstaller/skywars/6872265039.vapeprofile.txt", Method = 'GET'}).Body
+ 
+    writefile('vape/Profiles/6872274481.vapeprofile.txt', File1)
+    writefile('vape/Profiles/6872265039GUIPositions.vapeprofile.txt', File2)
+    writefile('vape/Profiles/6872265039.vapeprofile.txt', File3)
+     
+    notify("Install", "Successfully installed!")
+end
+
+function blue()
+    notify("Install", "Installing Prism V4 Blue client...")
+    local File1 = httprequest({Url = "https://raw.githubusercontent.com/Sail100/EtruiaConfigStorage/main/PrismInstaller/prismblue/6872274481.vapeprofile.txt", Method = 'GET'}).Body
+    local File2 = httprequest({Url = "https://raw.githubusercontent.com/Sail100/EtruiaConfigStorage/main/PrismInstaller/prismblue/6872265039GUIPositions.vapeprofile.txt", Method = 'GET'}).Body
+    local File3 = httprequest({Url = "https://raw.githubusercontent.com/Sail100/EtruiaConfigStorage/main/PrismInstaller/prismblue/6872265039.vapeprofile.txt", Method = 'GET'}).Body
+ 
+    writefile('vape/Profiles/6872274481.vapeprofile.txt', File1)
+    writefile('vape/Profiles/6872265039GUIPositions.vapeprofile.txt', File2)
+    writefile('vape/Profiles/6872265039.vapeprofile.txt', File3)
+     
     notify("Install", "Successfully installed!")
 end
 
@@ -152,12 +177,29 @@ function fakecloset()
     installfake()
 end
 
+function skywarsinstall()
+    wait(.5)
+    notify("Installer", "Starting Install of skywars client.")
+    ResetProfiles()
+    wait(1.1)
+    notify("Installer", "Deleted your Profiles Folder. Writing Profiles..")
+    sw()
+end
+
+function blueinstall()
+    wait(.5)
+    notify("Installer", "Starting Install of Prism V4 Blue client.")
+    ResetProfiles()
+    wait(1.1)
+    notify("Installer", "Deleted your Profiles Folder. Writing Profiles..")
+    blue()
+end
+
 function uninstall() 
    lplr:Kick("No uninstall method yet. Reinstall Render.")
 end
 
-
- local MTab = Window:MakeTab({
+local MTab = Window:MakeTab({
      Name = "Main Tab",
      Icon = "rbxassetid://4483345998",
      PremiumOnly = false
@@ -179,73 +221,83 @@ section("StatsS3", MTab, "Game Name: " ..game.Name)
  })
 
 
- section("TTabS1", TTab, "**MAKE SURE YOU HAVE RENDER INSTALLED WITH PROFILES**")
- section("TTabS2", TTab, "To install Prism, first go into the tab called: 'Install Prism' ")
- section("TTabS3", TTab, "Click on 'Install Prism' button inside the tab.")
- section("TTabS4", TTab, "Wait for it to finish installing and its done. Its that simple.")
+section("TTabS1", TTab, "**MAKE SURE YOU HAVE RENDER INSTALLED WITH PROFILES**")
+section("TTabS2", TTab, "To install Prism, first go into the tab called: 'Install Prism' ")
+section("TTabS3", TTab, "Click on 'Install Prism' button inside the tab.")
+section("TTabS4", TTab, "Wait for it to finish installing and its done. Its that simple.")
 
 
-  local ETab = Window:MakeTab({
-	    Name = "Install Prism",
-	    Icon = "rbxassetid://4483345998",
-        PremiumOnly = false
-    })
+local ETab = Window:MakeTab({
+	 Name = "Install Prism",
+	 Icon = "rbxassetid://4483345998",
+     PremiumOnly = false
+})
 
-   local UTab = Window:MakeTab({
-	    Name = "Uninstall Prism",
-	    Icon = "rbxassetid://4483345998",
-	    PremiumOnly = false
-    })
-
+local UTab = Window:MakeTab({
+    Name = "Uninstall Prism",
+	Icon = "rbxassetid://4483345998",
+	PremiumOnly = false
+})
     
-  ETab:AddButton({
-    	Name = "Install Prism",
-	    Callback = function()
-            install() 
-  	    end    
-    })
-
- ETab:AddButton({
-    	Name = "Install Prism Update 1",
-	    Callback = function()
-            install1() 
+ETab:AddButton({
+    Name = "Install Prism",
+	Callback = function()
+         install() 
   	end    
-    })
+ })
 
-
-
- ETab:AddButton({
-    	Name = "Install Prism Update 2",
-	    Callback = function()
-            install2() 
+ETab:AddButton({
+    Name = "Install Prism Update 1",
+	Callback = function()
+        install1() 
   	end    
-    })
+})
+
+ETab:AddButton({
+    Name = "Install Prism Update 2",
+	Callback = function()
+        install2() 
+  	end    
+})
     
-    ETab:AddButton({
-    	Name = "Install Prism Closet Cheating Client",
-	    Callback = function()
-            fakecloset() 
-  	end    
-    })
-    
+ETab:AddButton({
+    Name = "Install Prism Closet Cheating Client",
+	Callback = function()
+        fakecloset() 
+  end    
+})
+     
+ETab:AddButton({
+    Name = "Install Prism Skywars Client",
+	Callback = function()
+        skywarsinstall() 
+  end    
+})
    
-    
-   UTab:AddButton({
-   	Name = "Remove Prism",
-	   Callback = function()
-          uninstall()
-  	   end    
-   })
+ETab:AddButton({
+    Name = "Install Prism Blue Client",
+	Callback = function()
+        blueinstall() 
+  end    
+})
+
+UTab:AddButton({
+   Name = "Remove Prism",
+	Callback = function()
+        uninstall()
+  	end    
+})
 
     
-  local UUTab = Window:MakeTab({
-       Name = "Updates",
-	   Icon = "rbxassetid://4483345998",
-	   PremiumOnly = false
-  })
+local UUTab = Window:MakeTab({
+    Name = "Updates",
+	Icon = "rbxassetid://4483345998",
+	PremiumOnly = false
+})
 
 
 section("UUTabUpd1", UUTab, "2-3-24 | - Make Installer")
 section("UUTabUpd2", UUTab, "2-4-24 | - Update log on Github!")
 section("UUTabUpd3", UUTab, "2-9-24 | - Rewriten Code, fix, and new Installer Button!")
+section("UUTabUpd3", UUTab, "2-24-24 | - Http error fix, 2 new clients, formatting.")
 UUTab:AddLabel("More updates coming soon!")
