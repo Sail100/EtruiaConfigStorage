2381ea5b29725cb544a382a13a93996d3eb7efe1
Sail100 <85443860+Sail100@users.noreply.github.com>
2024-03-05 19:46:05 -0500

    update!

--- EtruiaDevelopment/loader.lua
+++ EtruiaDevelopment/loader.lua
@@ -1,15 +1,16 @@
 local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
-local Window = OrionLib:MakeWindow({Name = "Etruia Installation", HidePremium = false, IntroText = "Etruia installion UI is loading.."})
+local Window = OrionLib:MakeWindow({Name = "Etruia Installation", HidePremium = false, IntroText = "Etruia installation is loading.."})
 local lplr = game.Players.LocalPlayer
 local httpService = game:GetService('HttpService')
-local old = false
 local executor = (identifyexecutor or getexecutorname or function() return 'your executor' end)()
 local httprequest = (http and http.request or http_request or fluxus and fluxus.request or request or function() end)
 local isfile = isfile or function(file)
     local success, filecontents = pcall(function() return readfile(file) end)
     return success and type(filecontents) == 'string'
 end 
- 
+local VERISON = 'Installer Verison: v.1.3, Etruia lastest update: V2.30'
+print(VERISON)
+
 function notify(name, text)
   OrionLib:MakeNotification({
     Name = name,
@@ -24,6 +25,7 @@ function section(sectionname, tab, text)
    Name = text
 })
 end
+
 function testExecutor() 
 	if not isfile then
 		lplr:Kick("Executor does not support isfile, use Fluxus or Delta.")
@@ -47,11 +49,7 @@ function testExecutor()
 end
 
 testExecutor()
---[[
-if executor ~= 'Fluxus' then
-      lplr:Kick('Your executor: ' ..executor.. ' isnt supported! Use Fluxus.')
-end
--]]
+
 function ResetProfiles()
     if not isfolder('vape/Profiles') then
         makefolder('vape/Profiles')
@@ -63,38 +61,45 @@ function ResetProfiles()
     end
 end
 
+local complete = false
+
 function InstallProfiles()
-    local File1 = game:HttpGet("https://raw.githubusercontent.com/Sail100/EtruiaConfigStorage/main/EtruiaDevelopment/UpdateFile/Render_Blurple6872274481.vapeprofile.txt")
-    local File2 = game:HttpGet("https://raw.githubusercontent.com/Sail100/EtruiaConfigStorage/main/EtruiaDevelopment/UpdateFile/Render_Blurple6872265039.vapeprofile.txt")
-    local File3 = game:HttpGet("https://raw.githubusercontent.com/Sail100/EtruiaConfigStorage/main/EtruiaDevelopment/UpdateFile/6872274481.vapeprofiles.txt")
-    local File4 = game:HttpGet("https://raw.githubusercontent.com/Sail100/EtruiaConfigStorage/main/EtruiaDevelopment/UpdateFile/6872265039Render_BlurpleGUIPositions.vapeprofile.txt")
-    local File5 = game:HttpGet("https://raw.githubusercontent.com/Sail100/EtruiaConfigStorage/main/EtruiaDevelopment/UpdateFile/6872265039GUIPositions.vapeprofile.txt")
-    local File6 = game:HttpGet("https://raw.githubusercontent.com/Sail100/EtruiaConfigStorage/main/EtruiaDevelopment/UpdateFile/6872265039.vapeprofile.txt")
-	
-    writefile('vape/Profiles/Render_Blurple6872274481.vapeprofile.txt', File1)
-    writefile('vape/Profiles/Render_Blurple6872265039.vapeprofile.txt', File2)
-    writefile('vape/Profiles/6872274481.vapeprofiles.txt', File3)
-    writefile('vape/Profiles/6872265039Render_BlurpleGUIPositions.vapeprofile.txt', File4)
-    writefile('vape/Profiles/6872265039GUIPositions.vapeprofile.txt', File5)
-    writefile('vape/Profiles/6872265039.vapeprofile.txt', File6)
+    local File1 = httprequest({Url = 'https://raw.githubusercontent.com/Sail100/EtruiaConfigStorage/main/EtruiaDevelopment/updatefilenew/6872265039.vapeprofile.txt', Method ='GET'}).Body
+    local File2 = httprequest({Url = 'https://raw.githubusercontent.com/Sail100/EtruiaConfigStorage/main/EtruiaDevelopment/updatefilenew/6872265039GUIPositions.vapeprofile.txt', Method = 'GET'}).Body
+    local File3 = httprequest({Url = 'https://raw.githubusercontent.com/Sail100/EtruiaConfigStorage/main/EtruiaDevelopment/updatefilenew/6872265039Voidware%20PinkGUIPositions.vapeprofile.txt', Method = 'GET'}).Body
+    local File4 = httprequest({Url = 'https://raw.githubusercontent.com/Sail100/EtruiaConfigStorage/main/EtruiaDevelopment/updatefilenew/6872274481.vapeprofile.txt', Method ='GET'}).Body
+    local File5 = httprequest({Url = 'https://raw.githubusercontent.com/Sail100/EtruiaConfigStorage/main/EtruiaDevelopment/updatefilenew/6872274481.vapeprofiles.txt', Method ='GET'}).Body
+    local File6 = httprequest({Url = 'https://raw.githubusercontent.com/Sail100/EtruiaConfigStorage/main/EtruiaDevelopment/updatefilenew/Voidware%20Pink6872265039.vapeprofile.txt', Method ='GET'}).Body
+    local File7 = httprequest({Url = 'https://raw.githubusercontent.com/Sail100/EtruiaConfigStorage/main/EtruiaDevelopment/updatefilenew/Voidware%20Pink6872274481.vapeprofile.txt', Method ='GET'}).Body
+    
+    writefile('vape/Profiles/6872265039.vapeprofile.txt', File1)
+    writefile('vape/Profiles/6872265039GUIPositions.vapeprofile.txt', File2)
+    writefile('vape/Profiles/6872265039Voidware PinkGUIPositions.vapeprofile.txt', File3)
+    writefile('vape/Profiles/6872274481.vapeprofile.txt', File4)
+    writefile('vape/Profiles/6872274481.vapeprofiles.txt', File5)
+    writefile('vape/Profiles/Voidware Pink6872265039.vapeprofile.txt', File6)
+    writefile('vape/Profiles/Voidware Pink6872274481.vapeprofile.txt', File7)
+      
+      complete = true
+      
+      loadfile('vape/NewMainScript.lua')()
 end
 
-
 function install() 
-    print("Installing Etruia V2.1.1")
+    print("Installing Etruia V2.30")
     wait(.5)
-    notify("Installer", "Starting Install of Etruia. (v2.1.1)")
+    notify("Installer", "Starting Install of Etruia. (v2.30)")
     ResetProfiles()
     wait(1.1)
     notify("Installer", "Deleted your Profiles Folder. Writing Profiles..")
     InstallProfiles()
-    notify("Installer", "Sucessfully installed Etruia.")
+    notify("Installer", "Sucessfully installed Etruia. Enjoy!")
 end
-
 
 function uninstall() 
    lplr:Kick("No uninstall method yet. Reinstall Render.")
 end
+
 -- UI 
 
 local MTab = Window:MakeTab({
@@ -103,8 +108,6 @@ local MTab = Window:MakeTab({
     PremiumOnly = false
 })
     
- 
-
 section("StatsS01", MTab, "Welcome to Etruia's new Installer.")
 section("StatsS02", MTab, "Here, you install Etruia.")
 section("StatsS03", MTab, "If you need help, look at the Tutorial Tab.")
@@ -113,12 +116,12 @@ section("StatsS1", MTab, "Username: " ..lplr.Name)
 section("StatsS2", MTab, "ID: " ..lplr.UserId)
 section("StatsS3", MTab, "Game Name: " ..game.Name) 
 section("StatsS4", MTab, "Game ID: "..game.PlaceId)
+
 local TTab = Window:MakeTab({
     Name = "Tutorial",
     Icon = "rbxassetid://4483345998",
     PremiumOnly = false
 })
-
 
 section("TTabS1", TTab, "**MAKE SURE YOU HAVE RENDER INSTALLED WITH PROFILES**")
 section("TTabS2", TTab, "To install Etruia, first go into the tab called: 'Install Etruia' ")
@@ -137,12 +140,8 @@ local UTab = Window:MakeTab({
     PremiumOnly = false
 })
 
-
-section("Disclaimer", ETab, "Due to the rewrite on 2/3/2024, Etruia's past verisons have been removed.")
-
-    
 ETab:AddButton({
-Name = "Install Etruia",
+Name = "Install Etruia [Etruia V2.30],
 	 Callback = function()
           install()
   	end    
@@ -154,14 +153,12 @@ Name = "Remove Etruia",
         uninstall()
   	 end    
 })
-
     
 local UUTab = Window:MakeTab({
     Name = "Updates",
     Icon = "rbxassetid://4483345998",
     PremiumOnly = false
  })
-
 
 section("UUTabUpd1", UUTab, "1-16-24 | - Created github.")
 section("UUTabUpd2", UUTab, "1-17-24 | - Added V2 Installion")
@@ -172,5 +169,6 @@ section("UUTabUpd6", UUTab, "1/30/24 | - Updated Profiles, Fixed Github formatti
 section("UUTabUpd7", UUTab, "1/30/24 | - Updated Test Installer")	
 section("UUTabUpd8", UUTab, "2/3/24 | - Rewriten Etruia Installer")	
 section("UUTabUpd9", UUTab, "2-6-24 | Remake Etruia.")
+section("UUTabUpd10", UUTab, "3-5-24 | Remade Etruia, deprecated main loader.")
 UUTab:AddLabel("More updates coming soon!")
 OrionLib:Init()
